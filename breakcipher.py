adf139631565d6b7d03afb23e2f3b64b31d62cef
gsvieira <gsvieira98@gmail.com>
2021-08-12 13:09:43 -0400

    start

--- breakcipher.py
+++ breakcipher.py
@@ -2,10 +2,11 @@
 
 #Usa Ic pra determinar o tamanho da chave
 possible_keys = []
-sublists = []
+sublists = [0,0]
 def get_key_lenght(ciphertext):
   for i in range(2, 21):
+    chipher_list = list(ciphertext)
     for j in range (1, i+1):
-      print("x")
+      sublists [j]= chipher_list.pop(1)
 
 """use list pop to split the message into multiple substrings"""
