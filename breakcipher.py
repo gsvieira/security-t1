1ed30cd1a857ee67067f35c12b87fb8731d30695
gsvieira <gsvieira98@gmail.com>
2021-08-12 23:34:01 -0300

    fix sublist creation

--- breakcipher.py
+++ breakcipher.py
@@ -4,21 +4,25 @@ import string
 possible_keys = []
 def get_key_length(ciphertext):
   #divide a messagem em cosets de tamanho 2 a 20 para calculo de IC
-  for i in range(2, 21):
+  for i in range(2,8):
     cipher_list = list(ciphertext)
+    cipher_list_size = len(cipher_list)
     sublists = []
     IC_avg = 0.0
     #gera matrix de coset com tamanho entre 2 e 20
     for _ in range(i):
       sublists.append([])
     #separa a string nos cosets removendo o primeiro elemento da lista e adicionando ao sublist
-    for elem in cipher_list:
-      for j in range(i):
-        if cipher_list!=[]:
-          char= cipher_list.pop(0)
-          (sublists[j]).append(char)
-    for p in range(i):
-      print(sublists[p])
+    for j in range(cipher_list_size):
+    #while cipher_list!=[]:
+      #print(char,end=" ")
+      #for j in range(i):
+
+      char = cipher_list.pop(0)
+      (sublists[j%i]).append(char)
+    #test
+    """for p in range(i):
+      print(sublists[p])"""
     #calcula o IC
     for k in range(i):
       N = len(sublists[k])
@@ -29,7 +33,7 @@ def get_key_length(ciphertext):
       IC = freq_sum / (N*(N-1))
     #faz média entre os ICs das substrings
     IC_avg +=IC
-    possible_keys.append((i, IC_avg/i))
+    possible_keys.append((i, IC_avg))#acrecentar '/i'
   possible_keys.sort(key= lambda x: x[1], reverse = True)
   return possible_keys #retorna tamanho da chave com maior chance de ser a certa com o IC correspondente
 
@@ -38,7 +42,7 @@ def get_key(message, possible_keys):
   return 0
 
 
-f = open("output.txt", "r")
+f = open("ciphered.txt", "r")
 message = f.read()
 r = get_key_length(message)
 print(r)
