6e1d01f47ae51e1725f48f841575d74b1a75410b
gsvieira <gsvieira98@gmail.com>
2021-08-13 21:53:54 -0300

    Ui

--- breakcipher.py
+++ breakcipher.py
@@ -1,9 +1,9 @@
-import collections, string, re
+import collections, string, re, sys
 
 
 #Usa IC pra determinar o tamanho da chave
-possible_keys = []
 def get_key_length(ciphertext):
+  possible_keys = []
   #corrige espaços e outros caracteres não alphanumericos
   ciphertext_treated = re.sub("[^a-zA-Z]+","", ciphertext).lower()
   file = open("intermediario.txt","w")
@@ -45,8 +45,11 @@ def get_key_length(ciphertext):
   possible_keys.sort(key= lambda x: x[1], reverse = True)
   return possible_keys #retorna tamanho da chave com maior chance de ser a certa com o IC correspondente
 
-def get_key(ciphertext):
-  possible_keys = get_key_length(ciphertext)
+def get_key(ciphertext, key_lenght=0):
+  if not(int(key_lenght)):
+    possible_keys = get_key_length(ciphertext)
+  else:
+    possible_keys = [[int(key_lenght)]]
   ciphertext_treated = re.sub("[^a-zA-Z]+","", ciphertext).lower()
   alphabet_size = len(string.ascii_lowercase)
   cipher_list = list(ciphertext_treated)
@@ -56,8 +59,10 @@ def get_key(ciphertext):
   portuguese_letter_frequency = [0.1463, 0.0104, 0.0388, 0.0499, 0.1257, 0.0102, 0.013, 0.0128, 0.0618, 0.004, 0.0002, 0.0278, 0.0474, 0.0505, 0.1073, 0.0252, 0.012, 0.0653, 0.0781, 0.0434, 0.0463, 0.0167, 0.0001, 0.0021, 0.0001, 0.0047]
 
   sublists = []
-  key_result = []
-  result = ""
+  key_result_eng = []
+  key_result_pt = []
+  result_eng = ""
+  result_pt = ""
   #possible_keys = get_key_length(message)
   #divide a mensagem em cosets em quantidade igual ao tamanho da chave
   key_size = possible_keys[0][0]
@@ -68,7 +73,8 @@ def get_key(ciphertext):
     (sublists[j%key_size]).append(char)
   #faz para cada letra da chave
   for i in range(key_size):
-    x_square = []
+    x_square_eng = []
+    x_square_pt = []
     #tenta para todo shift possivel da letra
     for j in range(0,(-alphabet_size), -1):
       N = len(sublists[i])
@@ -82,43 +88,46 @@ def get_key(ciphertext):
           count_list.append((letter, 0))
       count_list.sort(key= lambda x: x[0])
       
-      f_sum = 0.0
+      f_sum_eng = 0.0
+      f_sum_pt = 0.0
       #print(len(count_list))
       for k in range (alphabet_size):
         freq = count_list[k][1]/N
-        f_sum+=((freq-english_letter_frequency[j+k])**2)/(english_letter_frequency[j+k])
-      x_square.append((-j ,f_sum))
-    x_square.sort(key= lambda x: x[1])
-    key_result.append(x_square[0][0])
-    result+=string.ascii_lowercase[x_square[0][0]]
-  return result
+        f_sum_eng+=((freq-english_letter_frequency[j+k])**2)/(english_letter_frequency[j+k])
+        f_sum_pt+=((freq-portuguese_letter_frequency[j+k])**2)/(portuguese_letter_frequency[j+k])
+      x_square_eng.append((-j ,f_sum_eng))
+      x_square_pt.append((-j ,f_sum_pt))
+    x_square_eng.sort(key= lambda x: x[1])
+    x_square_pt.sort(key= lambda x: x[1])
+    if verbose:
+      for l in range(1,3):
+        print(x_square_eng[l])
+        print(x_square_pt[l])
+    key_result_eng.append(x_square_eng[0][0])
+    key_result_pt.append(x_square_pt[0][0])
 
+    result_eng+=string.ascii_lowercase[x_square_eng[0][0]]
+    result_pt+=string.ascii_lowercase[x_square_pt[0][0]]
+  return (result_eng, result_pt)
 
-""" list1 = [('a',0)]
-for letter in string.ascii_lowercase:
-        try:
-          [x[0] for x in list1].index(letter)
-
-        except ValueError:
-          list1.append((letter, 0))
-print(list1) """
-
-
-
-
-"""f = open("ciphered.txt", "w")
-for letter in ("abc"):
-  for _ in range(7):
-    f.write(letter)
-f.close"""
-#file = open("intermediario.txt", "w")
-
-
-
-f = open("desafio2.txt", "r")
+#inicio do programa
+verbose = False
+if len(sys.argv)<=1:
+  print("É necessario o nome do arquivo que deseja abrir -> breakcipher.py nomedoarquivo.txt")
+  sys.exit()
+f = open(sys.argv[1], "r")
 message = f.read()
-#print(len(message))
-r = get_key(message)
-print(r)
+if len(sys.argv)==3:
+  if sys.argv[2]=="-v":
+    r = get_key(message)
+    verbose = True
+  else:
+    r = get_key(message, sys.argv[2])
+elif len(sys.argv)>3:
+  r = get_key(message, sys.argv[2])
+  if "-v"in sys.argv:
+    verbose = True
+else:
+  r = get_key(message)
+print("Possivel chave em ingles: {}\nPossivel chave em portugues: {}".format(r[0],r[1]))
 f.close
-#file.close
